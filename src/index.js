7a915efda565945359ebd5574c458823222d53a1
BooDzioo <boodzioo2@gmail.com>
2019-11-27 22:01:57 +0100

    added sequence of actions, negative numbers and removed some bugs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { number } from 'yargs';
-import { isNumberLiteral } from '@babel/types';
 
 class DeleteButton extends React.Component {
     constructor(props) {
@@ -26,7 +24,7 @@ class Operations extends React.Component {
         this.handleClick = this.handleClick.bind(this)
     }
     handleClick() {
-        this.props.handleOperation(this.props.value)
+        this.props.handleChange(this.props.value)
     }
     render() {
         return(
@@ -83,262 +81,263 @@ class CalcButton extends React.Component {
         )
     }
 }
+let numbers = []
+let signs = []
+let sum = []
+
+const baseNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+const baseSigns =                                                   //['^', 'x', '÷', '+', '-']
+{
+    '^': '^',
+    'x': 'x',
+    '÷': '÷',
+    '+': '+',
+    '-': '-'
+
+}
 
 class Calculator extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            currentValue: '',
-            previousValue: '',
-            method: '',
-            sumed: '',
-            operationHistory: '',
-            isFloat: false
+            current: '',
+            history: '',
+            sumed: ''
         }
         
         this.handleKeyDown = this.handleKeyDown.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.handleClearClick = this.handleClearClick.bind(this)
-        this.handleOperation = this.handleOperation.bind(this)
         this.handleSumClick = this.handleSumClick.bind(this)
-     //   this.delete = this.delete.bind(this)
-        this.add = this.add.bind(this)
-        this.sub = this.sub.bind(this)
-        this.mult = this.mult.bind(this)
-        this.div = this.div.bind(this)
-        this.pow = this.pow.bind(this)
+        this.delete = this.delete.bind(this)
     }
 
-    add(a, b) {
-        return a + b
-    }
+    handleChange(e) {
+        if (e in baseSigns) {
+            if (this.state.current.indexOf('.') !== -1) {
+                let v = this.state.indexOf('.')
+                let y = true
+                for (let i = this.state.current.length - 1; i > v; i--) {
+                    if (this.state.current.charAt(i) !== 0) {
+                        y = false
+                        break;
+                    }
+                    this.setState({
+                        current: this.state.current.slice(0, -1),
+                        history: this.state.history.slice(0, -1)
+                    })
+                }
+                if (y === true) {
+                    this.setState({
+                        current: this.state.current.slice(0, -1),
+                        history: this.state.history.slice(0, -1)
+                    })
+                }
+            }   else if (this.state.current !== '') {
+                    numbers.push(this.state.current)
+                    this.setState({
+                        current: ''
+                    })
+            }
 
-    sub(a, b) {
-        return a - b
-    }
-
-    mult(a, b) {
-        return a * b
-    }
-
-    div(a, b) {
-        return a / b
-    }
-
-    pow(a, b) {
-        return Math.pow(a, b)
-    }
-/*
-    delete() {
-        let currentValue = this.state.currentValue
-        let operationHistory = this.state.operationHistory
-        let sumed = this.state.sumed
-        let isFloat = this.state.isFloat
-        if (sumed !== '') {
-            this.setState({
-                sumed: ''
-            })
+            if (e === '-' && this.state.history.charAt(this.state.history.length - 1) !== '-') {
+                if (this.state.history === '' || this.state.history.charAt(this.state.history.lenght - 1) in baseSigns) {    //dodawanie minusa do liczby
+                    this.setState({
+                        current: `${this.state.current}${e}`,
+                        history: `${this.state.history}${e}`
+                    })
+                }   else {
+                    this.setState({
+                        history: `${this.state.history}${e}`
+                    })
+                    signs.push(e)
+                }
+            }   else {
+                    if (parseFloat(numbers[signs.length]) !== NaN) {
+                        if(this.state.history.charAt(this.state.history.length - 1) in baseSigns === false) {   //uniemożliwia wpisanie dwoch znaków pod rząd
+                            signs.push(e)
+                            this.setState({
+                                history: `${this.state.history}${e}`
+                            })
+                        }
+                    }
+            }
+        /*    if (this.state.sumed !== '') {
+                let sumed = this.state.sumed
+                this.setState({
+                    sumed: '',
+                    current: `${sumed}${e}`,
+                    history: `${sumed}${e}`
+                })
+            } */
         }
 
-        if (parseFloat(operationHistory.charAt(operationHistory.length - 1)) !== NaN) {
-            if (isFloat) {
-                if(operationHistory.charAt(operationHistory.length - 2) === '.') {
+
+        if (e === '.' && this.state.current.indexOf('.') === -1) {
+            if (this.state.current !== '') {    //uniemożliwienie wpisania kropki na początku
+                this.setState({
+                    current: `${this.state.current}${e}`,
+                    history: `${this.state.history}${e}`
+                })
+            }
+        }
+
+
+        if (e in baseNumbers) {
+            if (e === '0') {
+                if (this.state.history.charAt(this.state.history.length - 1) !== '') {  //uniemożliwienie wpisania 0 na początku
                     this.setState({
-                        isFloat: false,
-                        operationHistory: operationHistory.slice(0, -2),
+                        history: `${this.state.history}${e}`,
+                        current: `${this.state.current}${e}`
                     })
                 }
             }
+            if (e !== '0') {
+                this.setState({
+                    history: `${this.state.history}${e}`,
+                    current: `${this.state.current}${e}`
+                })
+            }
+        }
+    }
+
+    delete() {
+       if (this.state.history.charAt(this.state.history.length - 1) in baseSigns) {
+           if (this.state.current === this.state.history.charAt(this.state.history.length - 1)) {   //sprawdzanie czy liczba była ujemna
+                this.setState({
+                    current: '',
+                    history: this.state.history.slice(0, -1)
+                })    
+           }    else {
+                    this.setState({
+                        history: this.state.history.slice(0, -1)
+                    })
+                    signs.pop()
+           }
+       }
+
+
+       if (this.state.history.charAt(this.state.history.length - 1) === '.') {     //usuwanie kropek
             this.setState({
-                currentValue: Math.floor(currentValue / 10),
-                operationHistory: operationHistory.slice(0, -1)
+                history: this.state.history.slice(0, -1),
+                current: this.state.current.slice(0, -1)
             })
-        } else if (operationHistory.charAt(operationHistory.length - 1) === '.') {
+       }
+
+
+       if (this.state.history.charAt(this.state.history.length - 1) in baseNumbers) {
+           if (this.state.current === '') {
+               this.setState({
+                   current: numbers[numbers.length - 1]
+               })
+               numbers.pop()
+           }
+           this.setState({
+               current: this.state.current.slice(0, -1),
+               history: this.state.history.slice(0, -1)
+           })
+       }
+    }
+
+    handleSumClick() {
+        if (this.state.current.indexOf('.') !== -1) {
+            let v = this.state.indexOf('.')
+            let y = true
+            for (let i = this.state.current.length - 1; i > v; i--) {
+                if (this.state.current.charAt(i) !== 0) {
+                    y = false
+                    break;
+                }
+                this.setState({
+                    current: this.state.current.slice(0, -1),
+                    history: this.state.history.slice(0, -1)
+                })
+            }
+            if (y === true) {
+                this.setState({
+                    current: this.state.current.slice(0, -1),
+                    history: this.state.history.slice(0, -1)
+                })
+            }
+        }   else if (this.state.current !== '') {
+                numbers.push(this.state.current)
+                this.setState({
+                    current: ''
+                })
+        }
+
+        for (let x = 0; x < numbers.length; x++) {
+            let index = parseFloat(numbers[x])
+            sum.push(index)
+            sum.push(signs[x])
+            
+        }
+        console.log(numbers)
+        console.log(sum)
+        while(sum.findIndex((element) => element === '^') !== -1) {
+            let index = sum.findIndex((element) => element === '^')
+            sum[index - 1] = Math.pow(sum[index - 1], sum[index + 1])
+            sum.splice(index, 2)
+        }
+
+        while(sum.findIndex((element) => element === 'x') !== -1) {
+            let index = sum.findIndex((element) => element === 'x')
+            console.log('x')
+            sum[index - 1] = sum[index - 1] * sum[index + 1]
+            sum.splice(index, 2)
+        }
+
+        while(sum.findIndex((element) => element === '÷') !== -1) {
+            let index = sum.findIndex((element) => element === '÷')
+            console.log('/')
+            sum[index - 1] = sum[index - 1] / sum[index + 1]
+            sum.splice(index, 2)
+        }
+
+        while(sum.findIndex((element) => element === '+') !== -1) {
+            console.log('+')
+            let index = sum.findIndex((element) => element === '+')
+            sum[index - 1] = sum[index - 1] + sum[index + 1]
+            sum.splice(index, 2)
+        }
+
+        while(sum.findIndex((element) => element === '-') !== -1) {
+            console.log('-')
+            let index = sum.findIndex((element) => element === '-')
+            sum[index - 1] = sum[index - 1]  - sum[index + 1]
+            sum.splice(index, 2)
+        }
+        console.log(sum[0])
+        this.setState({
+            sumed: sum[0],
+        })
+        numbers = []
+        signs = []
+
+        if (sum[0] === undefined) {
             this.setState({
-                isFloat: false,
-                currentValue: currentValue.slice(0, -1),
-                operationHistory: operationHistory.slice(0, -1)
-            }) 
-        } else if (parseFloat(operationHistory.charAt(operationHistory.length - 1)) === NaN) {
-            this.setState({
-                method: '',
-                isSecond: false
+                sumed: 0
             })
         }
     }
-*/
-    handleSumClick() {
-
-        const method = this.state.method
-        const operationHistory = this.state.operationHistory
-
-       if(method !== ''){
-
-            let a = parseFloat(this.state.previousValue)
-            let b = parseFloat(this.state.currentValue)
-            let sum = a;
-
-            switch(method) {
-                case '+':
-                    sum = this.add(a, b)
-                    break;
-                case '-':
-                    sum = this.sub(a, b)
-                    break;
-                case '÷':
-                    sum = this.div(a, b)
-                    break;
-                case 'x':
-                    sum = this.mult(a ,b)
-                    break;
-                case '^':
-                    sum = this.pow(a, b)
-                    break;
-                default:
-                    break;
-           } 
-
-            this.handleClearClick()
-            this.setState({
-                operationHistory: operationHistory,
-                currentValue: sum,
-                sumed: sum 
-            })
-        }   
-    }
-    
-
-    handleOperation(e) {
-
-        let currentValue = this.state.currentValue
-        let previousValue = this.state.previousValue
-        let method = this.state.method
-        let sumed = this.state.sumed
-        let operationHistory = this.state.operationHistory
-        let sum = undefined;
-
-        if(currentValue !== '' && previousValue !== '') { 
-           if(method !== '') {
-
-                let a = parseFloat(this.state.previousValue)
-                let b = parseFloat(this.state.currentValue)
-
-                switch(method) {
-                    case '+':
-                        sum = this.add(a, b)
-                        break;
-                    case '-':
-                        sum = this.sub(a, b)
-                        break;
-                    case '÷':
-                        sum = this.div(a, b)
-                        break;
-                    case 'x':
-                        sum = this.mult(a ,b)
-                        break;
-                    case '^':
-                        sum = this.pow(a, b)
-                        break;
-                    default:
-                        break;
-            }
-            }   
-    }
-            if (isNaN(this.state.sumed) === false) {
-                this.setState({
-                    sumed: ''
-             })
-}
-
-        if (typeof currentValue === 'string') {
-            currentValue = parseFloat(currentValue)
-            } 
-
-        if (operationHistory !== '') {
-            if (isNaN(parseFloat(operationHistory.charAt(operationHistory.length - 1))) === true) {
-                operationHistory = operationHistory.slice(0, -1)
-                this.setState({
-                operationHistory: `${operationHistory}${e}`
-                })
-            } else {
-                this.setState({
-                    operationHistory: `${operationHistory}${e}`
-                }) }
-            }
-
-        if (this.state.currentValue !== '') {
-            this.setState({
-                previousValue: currentValue   
-        })
-    }       
-        
-        if (sum !== undefined) currentValue = sum
-
-        this.setState({
-            method: e,
-            isSecond: true,
-            isFloat: false
-    })
-}  
-    
 
     handleClearClick() {
-        this.setState({
-            operationHistory: '',
-            currentValue: '',
-            previousValue: '',
-            method: '',
-            sumed: ''
-        })
-    }
-    
-
-    handleChange(e) {
-
-        const sumed = this.state.sumed
-        let currentValue = this.state.currentValue
-        let method = this.state.method
-        let isSecond = this.state.isSecond
-        let isFloat = this.state.isFloat
-        let operationHistory = this.state.operationHistory
-
-        if (sumed !== '') {
-            this.setState({
-                previousValue: '',
-                sumed: ''
-            })
-            currentValue = ''
-            operationHistory = ''
-        }
-
-        if (e === '.' || isFloat === true) {
-            this.setState({
-                isFloat: true,
-                currentValue: `${currentValue}${e}`,
-                operationHistory: `${operationHistory}${e}`
-            })
-        } else {
-            let input = parseFloat(e)
-
-            if (currentValue === '' || isSecond === true) {
-                this.setState({
-                    currentValue: input,
-                    operationHistory: `${operationHistory}${input}`,
-                    isSecond: false
-            })
-            } else {
-                this.setState({
-                    currentValue: currentValue * 10 + input,
-                    operationHistory: `${operationHistory}${input}`    
-                })
-        } }
+       sum = []
+       signs = []
+       numbers = []
+       this.setState({
+        current: '',
+        history: '',
+        sumed: ''
+       })
     }
 
     handleKeyDown(e) {
         let keyDown = `${e.key}`
         console.log(keyDown)
         switch (keyDown) {
+            case 'del':
             case 'Escape':
                 this.handleClearClick()
                 break;
@@ -357,18 +356,16 @@ class Calculator extends React.Component {
             case '9':
             case '0':
             case '.':
-                this.handleChange(keyDown)
-                break;
             case '+':
             case '-':
             case '^':
-                this.handleOperation(keyDown)
+                this.handleChange(keyDown)
                 break; 
             case '*':
-                this.handleOperation('x')
+                this.handleChange('x')
                 break;
             case '/':
-                this.handleOperation('÷') 
+                this.handleChange('÷') 
             default:
                 break;
         }
@@ -384,7 +381,7 @@ class Calculator extends React.Component {
 
     render() {
 
-        const operationHistory = this.state.operationHistory
+        const operationHistory = this.state.history
         let sumed = ''
 
         if (this.state.sumed !== '') {
@@ -399,27 +396,27 @@ class Calculator extends React.Component {
                 </div>
                 <div className='boardRow'>
                     <ClearButton handleClearClick={this.handleClearClick}/>
-                    <DeleteButton /*delete={this.delete}*/ value='←'/>
-                    <Operations handleOperation={this.handleOperation} value='^'/>
-                    <Operations handleOperation={this.handleOperation} value='÷'/>
+                    <DeleteButton delete={this.delete} value='←'/>
+                    <Operations handleChange={this.handleChange} value='^'/>
+                    <Operations handleChange={this.handleChange} value='÷'/>
                 </div>
                 <div className='boardRow'>
                     <CalcButton value='1' handleChange={this.handleChange}/>
                     <CalcButton value='2' handleChange={this.handleChange}/>
                     <CalcButton value='3' handleChange={this.handleChange}/>
-                    <Operations handleOperation={this.handleOperation} value='x'/>
+                    <Operations handleChange={this.handleChange} value='x'/>
                 </div>
                 <div className='boardRow'>
                     <CalcButton value='4' handleChange={this.handleChange}/>
                     <CalcButton value='5' handleChange={this.handleChange}/>
                     <CalcButton value='6' handleChange={this.handleChange}/>
-                    <Operations handleOperation={this.handleOperation} value='-'/>
+                    <Operations handleChange={this.handleChange} value='-'/>
                 </div>
                 <div className='boardRow'>
                     <CalcButton value='7' handleChange={this.handleChange}/>
                     <CalcButton value='8' handleChange={this.handleChange}/>
                     <CalcButton value='9' handleChange={this.handleChange}/>
-                    <Operations handleOperation={this.handleOperation} value='+'/>
+                    <Operations handleChange={this.handleChange} value='+'/>
                 </div>
                 <div className='boardRow'>
                     <CalcButton value='0' handleChange={this.handleChange}/>
