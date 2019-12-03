6b15d3fc61f13bd064343f154905e077ef671431
BooDzioo <boodzioo2@gmail.com>
2019-12-03 15:52:47 +0100

    Updates

---
+++ precache-manifest.df52f208670d257f56443962b012a78b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "315a8b5f30d62eb38b314063d9c92d7b",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "b26f1f92f3a3d7827a95",
+    "url": "/react-calculator/static/css/main.40b5226b.chunk.css"
+  },
+  {
+    "revision": "41c5405b09b5a0e800d8",
+    "url": "/react-calculator/static/js/2.86c22323.chunk.js"
+  },
+  {
+    "revision": "b26f1f92f3a3d7827a95",
+    "url": "/react-calculator/static/js/main.b00003ae.chunk.js"
+  },
+  {
+    "revision": "6501a922dc5a4f91ca71",
+    "url": "/react-calculator/static/js/runtime-main.326eb139.js"
+  }
+]);
